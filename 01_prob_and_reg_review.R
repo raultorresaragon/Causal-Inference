771ec53129980c3dc65240132234113a255aa347
Raul Torres Aragon <raultorresaragon@icloud.com>
2024-12-25 20:24:38 -0500

    added reg to prob review

--- 01_prob_and_reg_review.R
+++ 01_prob_and_reg_review.R
@@ -25,3 +25,40 @@ sum(x)/sum(y)
 ((x-mean(x))^2) |> sum()
 
 
+# ---------- #
+# Regression #
+# ---------- #
+
+# Regression forces E(u|x) = E(u)
+x1 = ifelse(runif(n) <= 0.5, 1, 0)
+b0 = 1
+b1 = 2.5
+u = rnorm(n)
+y = b0 + b1*x1 + u
+fit <- lm(y~x1)
+e <- (y - predict(fit))
+sum(e)
+sum(e[x1==0])
+sum(e[x1==1])
+
+# If E(u)=0 and E(u|x)=0, then E(xu)=0 which means Cov(xu)=0
+# proof: E(xu) = E[E(xu|x)] = E[xE(u|x)] = E[x*0] = E(0) = 0
+cov(x1, e)
+mean(x1*e)
+
+# It still holds when the model is misspecified
+# E(u|x) = E(u) = 0 holds by design of regression 
+x2 <- rnorm(n) + 3*x1
+y = b0 + b1*x1 + 5*x2 + u
+fit <- lm(y~x1)
+e <- (y - predict(fit))
+sum(e)
+sum(e[x1==0])
+sum(e[x1==1])
+cov(x1, e)
+mean(x1*e)
+
+# Take away: sum(e)=0, mean(x1*e)=0, and sum(e|x)=0 is by construction of 
+# the \hat{beta}_k coefficients, and as such, regardless of model specification.
+
+
